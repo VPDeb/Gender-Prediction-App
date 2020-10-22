93e6dda423d380e695d3da06dae82ee5a90d2a34
vdeb <vdebebe924@gmail.com>
2020-10-21 21:05:12 -0400

    Update predictions.py

--- pages/predictions.py
+++ pages/predictions.py
@@ -10,7 +10,7 @@ import numpy as np
 import pandas as pd
 from app import app
 
-pipeline = load('../pipeline.joblib')
+pipeline = joblib.load('pages/pipeline.joblib')
 
 @app.callback(
     Output('prediction-content', 'children'),
